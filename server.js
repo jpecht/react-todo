d257e9c8c999cd4b50ba51a862df6dea98343185
jpecht <me.jefferson@gmail.com>
2017-12-05 16:51:02 -0700

    rearranged server to run by itself

--- server.js
+++ server.js
@@ -4,41 +4,16 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 
 const app = express();
-const port = 3000;
+const port = 8004;
 
-const compiler = webpack(config);
-const middleware = webpackMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  contentBase: 'src',
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false
-  }
-});
 
 app.use(bodyParser.json());
-
-// middleware that came with express-react-webpack boilerplate
-app.use(middleware);
-app.use(webpackHotMiddleware(compiler));
-app.get('*', function response(req, res) {
-  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-  res.end();
-});
-
-// middleware that came with jumpcloud ui template
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var whitelist = [
-    'http://localhost:3000',
+    'http://localhost:8005',
   ];
 
   var origin = req.headers.origin;
@@ -50,7 +25,8 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   return next();
-});
+});*/
+
 var data = {
   todos: require('./server/data/todos')
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,20 @@ const webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'client/main.js')
-  ],
+  entry: {
+    app: './client/main.js',
+  },
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
     publicPath: '/'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'client/index.tpl.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
+  devtool: 'source-map',
   module: {
-    loaders: [{
+    rules: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         "presets": ["react", "es2015", "stage-0", "react-hmre"]
       }
@@ -40,7 +26,54 @@ module.exports = {
       loader: 'json'
     }, {
       test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      loader: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      })),
+    }, {
+      test: /\.html/,
+      loader: 'html-loader',
+    },
+    {
+      test: /\.(jpe?g|png|gif)$/i,
+      loader: 'file-loader',
+      options: {
+        hash: 'sha512',
+        digest: 'hex',
+        name: 'images/[name].[ext]',
+      },
     }]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      ignoreOrder: true,
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['.css', '.js', '.jsx'],
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules',
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    hot: true,
+    port: 8005,
   }
 };
